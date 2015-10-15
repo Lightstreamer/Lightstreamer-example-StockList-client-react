9cba9ca2261021ee0de643afa235b18489f468a0
mone <simone.fabiano@gmail.com>
2015-10-15 13:09:27 +0200

    add r.js build file

---
+++ build_r.js/app.build.js
@@ -0,0 +1,33 @@
+({
+    appDir: "../src",
+    baseUrl: ".",
+    dir: "../built",
+    
+    //if using node to build the project this setting must be changed to "uglify"
+    optimize: "closure",
+    closure: {
+        CompilationLevel: 'SIMPLE_OPTIMIZATIONS',
+        loggingLevel: 'WARNING',
+    },
+    
+    skipDirOptimize: true,
+    
+    normalizeDirDefines: "skip",
+    
+    optimizeCss: "standard.keepLines",
+    
+    paths: {
+      "LightstreamerClient": "empty:",
+      "Subscription": "empty:",
+      "StatusWidget": "empty:"
+    },
+    
+    modules: [
+        {
+            name: "index",
+            includeRequire: false
+        }
+    ],
+    
+     preserveLicenseComments: false
+})
